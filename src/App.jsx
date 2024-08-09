3d270fefba4d55c14586af843f48a71780502a62
unknown <auyoncep16@gmail.com>
2024-08-10 00:03:59 +0600

    added add product form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import DenseAppBar from './components/AppBar'
+import AddProductsForm from './components/AddProductsForm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <DenseAppBar/>
+      <h1> Add Products</h1>
+      <AddProductsForm/>
     </>
   )
 }
